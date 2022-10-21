bfa7ff5a9d80a767fe4104da612e1fe5b68f74cd
H-Yuuki <yuukirinnma@gmail.com>
2022-10-22 00:18:46 +0900

    add: TDDの手順をコメントで記載

--- Unit/UnitTestApp01/UnitTestApp01/ViewController.swift
+++ Unit/UnitTestApp01/UnitTestApp01/ViewController.swift
@@ -5,6 +5,19 @@
 //  Created by 濵田　悠樹 on 2022/10/21.
 //
 
+/*
+ 
+    ポーカーアプリを作ると仮定して、TDD(テスト駆動開発)に取り組む
+    
+    手順
+        1: プロジェクト作成
+        2: 何を実装するか仕様書となる TODO を作成
+            - 仕様漏れに気づいたらその都度追加する
+        3: 「Command + U」でテストを実行, 実行環境によるエラーが発生しないことを確認
+        4:
+ 
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
