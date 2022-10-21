0f0a306798c120edc4f2710397a98d2d1abb995c
H-Yuuki <yuukirinnma@gmail.com>
2022-10-22 00:22:54 +0900

    add: テストコードを正常に実行できるよう Card を作成

---
+++ Unit/UnitTestApp01/UnitTestApp01/Card.swift
@@ -0,0 +1,22 @@
+//
+//  Card.swift
+//  UnitTestApp01
+//
+//  Created by 濵田　悠樹 on 2022/10/22.
+//
+
+import Foundation
+
+struct Card {
+    enum Suit {
+        case heart
+    }
+    
+    enum Rank {
+        case three
+    }
+    
+    let suit: Suit
+    let rank: Rank
+}
+
