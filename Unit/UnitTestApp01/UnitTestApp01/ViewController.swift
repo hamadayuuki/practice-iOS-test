28694a9b8d0b1b8081c8d9feade4f9e4d846a30e
H-Yuuki <yuukirinnma@gmail.com>
2022-10-21 23:56:14 +0900

    add: 初期ファイル作成

---
+++ Unit/UnitTestApp01/UnitTestApp01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTestApp01
+//
+//  Created by 濵田　悠樹 on 2022/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+
+
+}
+
