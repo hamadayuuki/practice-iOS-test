03e99b84c5bb4e265b6664fb0f5b4c08300d9a23
hamadayuuki <yuukirinnma@gmail.com>
2023-03-04 19:59:53 +0900

    add: テストコード

---
+++ Unit/UnitTestApp01/UnitTestApp01Tests/CalculationNumberTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalculationNumberTests.swift
+//  UnitTestApp01Tests
+//
+//  Created by 濵田　悠樹 on 2023/03/04.
+//
+
+import XCTest
+@testable import UnitTestApp01   // テスト対象のモジュールを指定, テスト対象のCalculationNumberクラスはUnitTestApp01モジュールと依存関係にあるので、今回はモジュールダ名がフォルダ名が
+
+final class CalculationNumberTests: XCTestCase {
+    func test_足し算_成功() {
+        let calculationNumber = CalculationNumber()   // 足し算を行うためにインスタンス化
+        let answer = calculationNumber.Add(a: 10, b: 20)   // 足し算, answer = 30
+        XCTAssertEqual(answer, 30)   // answer と 30 が等しいかテスト
+    }
+}
