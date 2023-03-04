eb894d5f7dcb3caf6307abed9eecb8d985f54b70
hamadayuuki <yuukirinnma@gmail.com>
2023-03-04 19:22:28 +0900

    add: テスト対象のプログラム

---
+++ Unit/UnitTestApp01/UnitTestApp01/CalculationNumber.swift
@@ -0,0 +1,14 @@
+//
+//  CalculationNumber.swift
+//  UnitTestApp01
+//
+//  Created by 濵田　悠樹 on 2023/03/04.
+//
+
+import Foundation
+
+class CalculationNumber {
+    func Add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
